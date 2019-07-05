f45d4593f6242ecb6a8a1f0fd95b7dbb5cd4d287
KrisztinaFoldi <krisztifoldi11@gmail.com>
2019-07-05 11:16:29 +0200

    diamonds in a triangle

---
+++ week-02/day-4/src/Triangles.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Triangles {
+    public static void mainDraw(Graphics graphics) {
+
+        triangle(graphics);
+
+
+
+    }
+
+    public static void triangle ( Graphics graphics) {
+        for (int i = 0; i < 32; i++) {
+            graphics.drawLine(160-5*i, 0+10*i, 320-10*i, 320);
+            graphics.drawLine(160+5*i, 0+10*i,0+10*i, 320);
+
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
