d215de3c730a68a74c11d25b4732adf25d62b9fc
KrisztinaFoldi <krisztifoldi11@gmail.com>
2019-07-09 12:01:04 +0200

    for loop added to print each line

--- week-03/day-2/src/PrintEachLine.java
+++ week-03/day-2/src/PrintEachLine.java
@@ -6,9 +6,11 @@ import java.util.List;
 public class PrintEachLine {
   public static void main(String[] args) {
     try {
-      Path filePath = Paths.get("my - file.txt");
+      Path filePath = Paths.get("/Users/foldikrisztina/greenfox/KrisztinaFoldi/week-03/day-2/src/index.txt");
       List<String> lines = Files.readAllLines(filePath);
-      System.out.println(lines);
+      for (int i = 0; i < lines.size(); i++) {
+        System.out.println(lines.get(i));
+      }
       } catch (Exception e) {
       System.out.println("Unable to read file: my-file.txt");
       }
