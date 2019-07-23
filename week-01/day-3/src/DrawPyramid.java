72c1cb514c7cee9ab286aa598ea24ec431f74213
KrisztinaFoldi <krisztifoldi11@gmail.com>
2019-07-23 12:55:12 +0200

    practice for exam

--- week-01/day-3/src/DrawPyramid.java
+++ week-01/day-3/src/DrawPyramid.java
@@ -6,12 +6,12 @@ public class DrawPyramid {
         int height = scanner.nextInt();
         int star = 0;
 
-        for ( int a = 1; a <= height; a += 1) {
-            for ( int space = 1; space <= (height - a ); space++) {
+        for ( int i = 1; i <= height; i += 1) {
+            for ( int space = 1; space <= (height - i ); space++) {
                 System.out.print(" ");
             }
-            while ( star != a * 2 - 1){
-                System.out.print("*");
+            for ( star = 0; star < i * 2 - 1; star++){
+                System.out.print("* ");
                 star ++;
             }
             System.out.println();
