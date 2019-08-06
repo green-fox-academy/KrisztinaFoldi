1fe30b9399496ebced6235cb54b9d992455e966d
KrisztinaFoldi <krisztifoldi11@gmail.com>
2019-08-06 17:23:09 +0200

    greets me and counts the times from html

--- week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,14 +3,20 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 
 @Controller
 public class HelloWebController {
+  AtomicLong number = new AtomicLong();
 
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", "World");
+  public String greeting(Model model, @RequestParam(value = "name") String name) {
+    long theValue = number.incrementAndGet();
+    model.addAttribute("name", name);
+    model.addAttribute("counter", theValue );
     return "greeting";
   }
 }
