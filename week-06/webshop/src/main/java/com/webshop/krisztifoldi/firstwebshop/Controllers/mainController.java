ec861c060a62c5a1fbe3100959905bb9e096ee72
KrisztinaFoldi <krisztifoldi11@gmail.com>
2019-08-08 14:50:46 +0200

    can filter only available items

--- week-06/webshop/src/main/java/com/webshop/krisztifoldi/firstwebshop/Controllers/mainController.java
+++ week-06/webshop/src/main/java/com/webshop/krisztifoldi/firstwebshop/Controllers/mainController.java
@@ -6,17 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Controller
 public class mainController {
-  ShopItem runningShoes = new ShopItem("Running shoes", "Nike running shoes for every day sport", 100, 5);
-  ShopItem printer = new ShopItem("Printer", "Some HP printer that will print pages", 300, 2);
-  ShopItem cocaCola = new ShopItem("Coca Cola", "0,5l standard coke", 3, 0);
-  ShopItem tShirt = new ShopItem("T-Shirt", "Blue with a corgie on a bike", 20, 1);
+  List<ShopItem> items = new ArrayList<>();
+
 
   public List<ShopItem> fillTheListWithItems() {
-    List<ShopItem> items = new ArrayList<>();
+    ShopItem runningShoes = new ShopItem("Running shoes", "Nike running shoes for every day sport", 100, 5);
+    ShopItem printer = new ShopItem("Printer", "Some HP printer that will print pages", 300, 2);
+    ShopItem cocaCola = new ShopItem("Coca Cola", "0,5l standard coke", 3, 0);
+    ShopItem tShirt = new ShopItem("T-Shirt", "Blue with a corgie on a bike", 20, 1);
     items.add(runningShoes);
     items.add(printer);
     items.add(cocaCola);
@@ -27,12 +29,22 @@ public class mainController {
 
   @GetMapping("")
   public String mainPage(Model model){
+    items.clear();
     fillTheListWithItems();
-    model.addAttribute("name" , runningShoes.getName());
+    model.addAttribute("items" , items);
     return "webshop";
   }
 
-
+  @GetMapping("/only-available")
+  public String availablePage(Model model){
+    items.clear();
+    fillTheListWithItems();
+    List<ShopItem> availableItems = items
+            .stream().filter(x -> x.getQuantityOfStock() > 0)
+            .collect(Collectors.toList());
+    model.addAttribute("items" , availableItems);
+    return "webshop";
+  }
 
 
 
